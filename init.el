7651294c8c892a7f2b0f26a2c067762ff66ed17b
Rafal Przywarski <rafal.przywarski@gmail.com>
2018-05-19 19:52:28 +0200

    config for Clojure

---
+++ init.el
@@ -0,0 +1,52 @@
+(require 'package)
+(setq exec-path (append exec-path '("/usr/local/bin")))
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(require 'smooth-scrolling)
+(smooth-scrolling-mode 1)
+
+(setq mouse-wheel-scroll-amount '(1))
+
+(set-language-environment "UTF-8")
+(set-default-font "Source Code Pro-14")
+
+(setq-default comment-column 70)
+(setq-default line-spacing 2)
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(tool-bar-mode -1)
+(toggle-scroll-bar -1)
+
+;; Enter cider mode when entering the clojure major mode
+(add-hook 'clojure-mode-hook 'cider-mode)
+
+;; Turn on auto-completion with Company-Mode
+(global-company-mode)
+(add-hook 'cider-repl-mode-hook #'company-mode)
+(add-hook 'cider-mode-hook #'company-mode)
+
+;; Replace return key with newline-and-indent when in cider mode.
+(add-hook 'cider-mode-hook '(lambda () (local-set-key (kbd "RET") 'newline-and-indent)))
+
+(show-paren-mode 1)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/lib/monokai-theme")
+(load-theme 'monokai t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit paredit projectile smooth-scrolling helm-ag highlight-symbol rainbow-delimiters monokai-theme company cider))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
