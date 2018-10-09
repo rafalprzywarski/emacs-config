a62c3bab6980099a6de514cfb3f9f35dc2c2246d
Rafal Przywarski <rafal.przywarski@gmail.com>
2018-10-09 22:52:59 +0200

    added markdown-mode

--- init.el
+++ init.el
@@ -56,9 +56,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(markdown-command "pandoc")
  '(package-selected-packages
    (quote
-    (magit paredit projectile smooth-scrolling helm-ag highlight-symbol rainbow-delimiters monokai-theme company cider))))
+    (markdown-mode magit paredit projectile smooth-scrolling helm-ag highlight-symbol rainbow-delimiters monokai-theme company cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
